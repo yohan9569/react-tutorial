b7059b335e03ea6b40f3d30f4790b373c56a0c43
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2025-01-07 17:40:49 +0900

    FEAT: 2번 실습

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import { useState } from 'react'
-import reactLogo from '@/assets/react.svg'
-import viteLogo from '/vite.svg'
 import '@/App.css'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [level, setLevel] = useState(1)
+  const [title, setTitle] = useState('Novice')
+
+  const levelUp = () => {
+    setLevel((previous) => {
+      const newVal = previous + 1
+
+      if (newVal == 30) setTitle('2차 전직')
+      else if (newVal == 15) setTitle('1차 전직')
+
+      return newVal
+    })
+  }
 
   return (
-    // <React.Fragment></React.Fragment> == <Fragment></Fragment> == <></>
-    // jsx는 무조건 하나의 태그만 있어야 해. 그래서 최상단 껍데기 둠.
-    // jsx 내부에서 js 문법 -> 무조건 {} 중괄호.
     <>
-      <div>{count}</div>
-      <button onClick={(e) => setCount(count + 1)}>증가</button>
-      <button onClick={(e) => setCount(count - 1)}>감소</button>
+      <div style={{ marginBottom: 10 }}>{level}</div>
+      <div style={{ marginBottom: 10 }}>{title}</div>
+      <button onClick={levelUp}>레벨업!</button>
     </>
   )
 }
