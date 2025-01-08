cf034064ef18def210d5da8330b8575da04fef30
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2025-01-08 14:01:34 +0900

    FEAT: 4-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,38 @@
+/*
+[**실습 4] : 앞선 `state` 와 `setState` 예시에서 `count` 와 `button` 두 개 각각 컴포넌트 만들어보자**
+
+- 아래의 코드에서 `count` 를 포함한 `<div>` 를 리액트 함수형 컴포넌트로 만들어라
+- 아래의 코드에서 `<button>` 를 리액트 함수형 컴포넌트로 만들어라
+    1. 증가 버튼과 감소 버튼 각각 다른 컴포넌트로 만들어라
+    2. 단일 버튼 컴포넌트를 만들어서 각각 증가와 감소 버튼으로 활용해보아라
+*/
+
 import '@/App.css'
 import { useState } from 'react'
 
-function App() {
-  const [age, setAge] = useState(0)
-  const [valid, setValid] = useState(false)
+function CountCompo({ count }) {
+  // React 컴포넌트는 대문자로 시작해야 한다!
+  return <div style={{ marginBottom: 10 }}>{count}</div>
+}
 
-  const handleInput = (e) => {
-    const input = e.currentTarget.value
-    setAge(input)
-    setValid(input >= 19)
-  }
+function UpButton({ setCount }) {
+  return <button onClick={() => setCount((prev) => prev + 1)}>증가</button>
+}
+
+function DownButton({ setCount }) {
+  return <button onClick={() => setCount((prev) => prev - 1)}>감소</button>
+}
+
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <input type='number' value={age} onChange={handleInput} />
-      {valid ? <div>성년</div> : <div>미성년</div>}
+      <CountCompo count={count} />
+      <div style={{ display: 'flex', flexDirection: 'row', gap: 10 }}>
+        <UpButton setCount={setCount} />
+        <DownButton setCount={setCount} />
+      </div>
     </>
   )
 }
