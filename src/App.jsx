85073e54cd493b31d991456591e14e7aace5a0c5
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2025-01-13 20:29:10 +0900

    FEAT: [실습 10-2] useRef 통한 <input> 의 React Component 화

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,49 @@
 /*
-[실습 10] 불필요한 리렌더링 없는 Validation 을 갖춘 이름 / 설명 / 메일 폼 제출 페이지를 만들어라
-- 가장 먼저 이것저것 생각하지말고 최소한 불필요한 리렌더링 없이 동작되는 형태로 개발하라
+[실습 10-2] useRef 통한 <input> 의 React Component 화
 */
 
 import { useState, useRef } from 'react'
 import '@/App.css'
 
-function App() {
-  // const [valid, setValid] = useState(false)
-  const nameRef = useRef()
-  const descRef = useRef()
-  const mailRef = useRef()
+function FormWithValidation({ label, required = false, length = undefined }) {
+  const reference = useRef()
 
-  const [required, setRequired] = useState(true)
+  const [requiredValid, setRequiredValid] = useState(true)
   const [lengthValid, setLengthValid] = useState(true)
 
-  const handleSubmit = () => {
-    const name = nameRef?.current?.value
-    const desc = descRef?.current?.value
-    const mail = mailRef?.current?.value
+  console.log(`${label} FWV - rerendered`)
 
-    console.log(name, desc, mail)
-  }
-
-  console.log('- rerendered')
   return (
     <>
-      이름:{' '}
-      <input
-        ref={nameRef}
-        type='text'
-        name='name'
-        onChange={(e) => {
-          setRequired(e.target.value.length !== 0)
-          setLengthValid(e.target.value.length < 10)
-        }}
-      />
-      {lengthValid || <div style={{ color: 'red' }}>길이 준수</div>}
-      {required || <div style={{ color: 'red' }}>필수 항목</div>}
-      설명: <input ref={descRef} type='text' name='desc'></input>
-      메일: <input ref={mailRef} type='email' name='mail'></input>
-      <button type='button' onClick={handleSubmit}>
-        제출
-      </button>
+      <div>
+        {label}:{' '}
+        <input
+          ref={reference}
+          type='text'
+          onChange={(e) => {
+            if (required) setRequiredValid(e.target.value.length !== 0)
+            if (length) setLengthValid(e.target.value.length <= length)
+          }}
+        />
+        {lengthValid || (
+          <div style={{ color: 'red' }}>
+            {label}길이 준수{length}
+          </div>
+        )}
+        {requiredValid || <div style={{ color: 'red' }}>필수 항목</div>}
+      </div>
+    </>
+  )
+}
+
+function App() {
+  console.log('App - rerendered')
+  return (
+    <>
+      <FormWithValidation label='이름' required={true} length={10} />
+      <FormWithValidation label='설명' />
+      <FormWithValidation label='메일' required />
+      <button type='button'>제출</button>
     </>
   )
 }
