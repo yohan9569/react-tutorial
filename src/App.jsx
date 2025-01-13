6cb8a197a5fe6bc33954a1e8dfaa1f003e6ad6a9
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2025-01-13 21:51:34 +0900

    FEAT: [12-4] 유효성 검증을 위한 상태 단일화 (불변성 객체로 인한 리렌더 이슈) -> onChange 마다 새 객체로 setState

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 /*
 실습 12 : Ref 와 State 를 조합하여, 유효성 검증에 따른 포커스까지 도입하여 회원가입 페이지 만들기
-[12-3] 유효성 검증을 위한 상태 생성 및 검증 함수 생성
+[12-4] 유효성 검증을 위한 상태 단일화 (불변성 객체로 인한 리렌더 이슈) -> onChange 마다 새 객체로 setState
 */
 
 import { useState, useRef, forwardRef } from 'react'
@@ -16,9 +16,11 @@ function IdInput() {
 
 function PwInput() {
   const reference = useRef()
-  const [requiredValid, setRequiredValid] = useState(false)
-  const [minLengthValid, setMinLengthValid] = useState(false)
-  const [maxLengthValid, setMaxLengthValid] = useState(true)
+  const [valid, setValid] = useState({
+    required: false,
+    min: false,
+    max: true,
+  })
 
   function changType(e) {
     if (reference.current.type === 'password') {
@@ -30,6 +32,8 @@ function PwInput() {
     }
   }
 
+  console.log('PW- rerendered')
+
   return (
     <div>
       PW:{' '}
@@ -38,21 +42,23 @@ function PwInput() {
         ref={reference}
         onChange={(e) => {
           const length = e.target.value.length
-          setRequiredValid(length !== 0)
-          setMinLengthValid(length > 8)
-          setMaxLengthValid(length < 20)
+          setValid({
+            required: length !== 0,
+            min: length > 8,
+            max: length < 20,
+          })
         }}
       />
       <button onClick={changType}>보이기</button>
-      {requiredValid || <div style={{ color: 'red' }}>필수 항목</div>}
-      {minLengthValid || <div style={{ color: 'red' }}>최소 8 글자</div>}
-      {maxLengthValid || <div style={{ color: 'red' }}>최대 20 글자</div>}
+      {valid.required || <div style={{ color: 'red' }}>필수 항목</div>}
+      {valid.min || <div style={{ color: 'red' }}>최소 8 글자</div>}
+      {valid.max || <div style={{ color: 'red' }}>최대 20 글자</div>}
     </div>
   )
 }
 
 function App() {
-  console.log('- rerendered')
+  console.log('APP- rerendered')
   function registration() {}
 
   return (
