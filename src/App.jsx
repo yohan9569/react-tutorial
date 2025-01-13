2d587d11f0c9275cbdeee72fa20654446bb1579e
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2025-01-13 20:46:17 +0900

    FEAT: [실습 10-3] 부모 컴포넌트에서 모든 useRef 를 갖고 일괄 제출 (forwardRef 버그)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 /*
-[실습 10-2] useRef 통한 <input> 의 React Component 화
+[실습 10-3] 부모 컴포넌트에서 모든 useRef 를 갖고 일괄 제출 (forwardRef 버그)
 */
 
 import { useState, useRef } from 'react'
 import '@/App.css'
 
-function FormWithValidation({ label, required = false, length = undefined }) {
-  const reference = useRef()
-
+function FormWithValidation({ label, required = false, length = undefined }, reference) {
   const [requiredValid, setRequiredValid] = useState(true)
   const [lengthValid, setLengthValid] = useState(true)
 
@@ -37,12 +35,27 @@ function FormWithValidation({ label, required = false, length = undefined }) {
 }
 
 function App() {
+  const references = {
+    name: useRef(),
+    desc: useRef(),
+    mail: useRef(),
+  }
+
+  function submit() {
+    console.log({
+      name: references?.name?.current?.value,
+      desc: references?.desc?.current?.value,
+      mail: references?.mail?.current?.value,
+    })
+  }
+
   console.log('App - rerendered')
   return (
     <>
-      <FormWithValidation label='이름' required={true} length={10} />
-      <FormWithValidation label='설명' />
-      <FormWithValidation label='메일' required />
+      <FormWithValidation ref={references.name} label='이름' required={true} length={10} />
+      <FormWithValidation ref={references.desc} label='설명' length={20} />
+      <FormWithValidation ref={references.mail} label='메일' required />{' '}
+      {/* 값을 명시하지 않은 boolean props = true로 가정. 아예 언급 없으면 undefined*/}
       <button type='button'>제출</button>
     </>
   )
