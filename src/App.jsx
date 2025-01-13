d605ac845124efd6233ffd2263daa74663639fbd
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2025-01-13 21:12:47 +0900

    FEAT: [11-1] useRef 통해 원하는 DOM(HTML 요소)과 연결 - 사과 색 바꾸기

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,26 @@
 /*
-[실습 10-4] 함수형 컴포넌트에선 forwardRef 가 필수
+실습 11 : 렌더링 없이도 ref 를 통해 DOM 조작이 가능하다는것을 직접 코드를 통해 이해해보자
+[11-1] useRef 통해 원하는 DOM(HTML 요소)과 연결
 */
 
 import { useState, useRef, forwardRef } from 'react'
 import '@/App.css'
 
-const FormWithValidation = forwardRef(function FormWithValidation(
-  { label, required = false, length = undefined },
-  reference,
-) {
-  const [requiredValid, setRequiredValid] = useState(true)
-  const [lengthValid, setLengthValid] = useState(true)
-
-  console.log(`${label} FWV - rerendered`)
-
-  return (
-    <>
-      <div>
-        {label}:{' '}
-        <input
-          ref={reference}
-          type='text'
-          onChange={(e) => {
-            if (required) setRequiredValid(e.target.value.length !== 0)
-            if (length) setLengthValid(e.target.value.length <= length)
-          }}
-        />
-        {lengthValid || (
-          <div style={{ color: 'red' }}>
-            {label}길이 준수{length}
-          </div>
-        )}
-        {requiredValid || <div style={{ color: 'red' }}>필수 항목</div>}
-      </div>
-    </>
-  )
-})
-
 function App() {
-  const references = {
-    name: useRef(),
-    desc: useRef(),
-    mail: useRef(),
-  }
+  const appleRef = useRef()
+  console.log('- rerendered')
 
-  function submit() {
-    console.log({
-      name: references?.name?.current?.value,
-      desc: references?.desc?.current?.value,
-      mail: references?.mail?.current?.value,
-    })
-  }
-
-  console.log('App - rerendered')
   return (
     <>
-      <FormWithValidation ref={references.name} label='이름' required={true} length={10} />
-      <FormWithValidation ref={references.desc} label='설명' length={20} />
-      <FormWithValidation ref={references.mail} label='메일' required />{' '}
-      {/* 값을 명시하지 않은 boolean props = true로 가정. 아예 언급 없으면 undefined*/}
-      <button type='button'>제출</button>
+      <div ref={appleRef}>apple</div>
+      <button
+        onClick={() =>
+          (appleRef.current.style.color =
+            appleRef.current.style.color === 'black' ? 'red' : 'black')
+        }
+      >
+        변경
+      </button>
     </>
   )
 }
