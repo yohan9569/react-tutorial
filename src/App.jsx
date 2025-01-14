0c691a6e2a6ec46cf59f45fbbd338401e3f2355d
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2025-01-14 10:20:00 +0900

    FEAT: [13-1] 블로그 포스트 페이지 내 컴포넌트 분리, Props 로 컴포넌트 재사용성 개선

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,45 @@
 /*
-실습 12 : Ref 와 State 를 조합하여, 유효성 검증에 따른 포커스까지 도입하여 회원가입 페이지 만들기
-[12-5] immer: 객체 상태 내 프로퍼티 단위 불변성 보장으로 리렌더 이슈 수정
-npm install immer --save: immer package.json dependencies 자동 추가, -lock도 반영.
+실습 13 : Ref 활용하여, 블로그에서 글 읽을때 어떤 제목의 글인지 상단 내비게이션 헤더에 제목 표기
+[13-1] 블로그 포스트 페이지 내 컴포넌트 분리, Props 로 컴포넌트 재사용성 개선
 */
 
 import { useState, useRef, forwardRef } from 'react'
 import '@/App.css'
 import { produce } from 'immer'
 
-function IdInput() {
-  return (
-    <div>
-      ID: <input />
-    </div>
-  )
+function Header() {
+  return <div style={{ position: 'sticky', top: 0, height: 60, backgroundColor: 'white' }}></div>
 }
 
-function PwInput({ setChecked }) {
-  const [valid, setValid] = useState({
-    required: false,
-    min: false,
-    max: true,
-  })
-  const reference = useRef() //순서에 대한 공식 규칙은 없지만, useState → useRef → 기타 훅의 순서를 많이 따릅니다.
+function Title({ title }) {
+  return <h3>{title}</h3>
+}
 
-  function changType(e) {
-    if (reference.current.type === 'password') {
-      reference.current.type = 'text'
-      e.target.value = '감추기'
-    } else {
-      reference.current.type = 'password'
-      e.target.value = '보이기'
-    }
-  }
+function Content({ content }) {
+  return <div>{content}</div>
+}
 
-  console.log('PW- rerendered')
-
+function Post({ title, content }) {
   return (
-    <div>
-      PW:{' '}
-      <input
-        type='password'
-        ref={reference}
-        onChange={(e) => {
-          const length = e.target.value.length
-          const changed = produce(valid, (draft) => {
-            draft.required = length !== 0
-            draft.min = length > 8
-            draft.max = length < 20
-          })
-          setValid(changed)
-          setChecked(changed.required && changed.min && changed.max)
-        }}
-      />
-      <button onClick={changType}>보이기</button>
-      {valid.required || <div style={{ color: 'red' }}>필수 항목</div>}
-      {valid.min || <div style={{ color: 'red' }}>최소 8 글자</div>}
-      {valid.max || <div style={{ color: 'red' }}>최대 20 글자</div>}
-    </div>
+    <>
+      <Title title={title} />
+      <Content content={content} />
+    </>
   )
 }
 
 function App() {
-  const [checked, setChecked] = useState(false)
-  console.log('APP- rerendered')
-  function registration() {}
+  const post = {
+    title: 'Lorem ipsum',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, \
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  }
 
   return (
     <>
-      <IdInput />
-      <PwInput setChecked={setChecked} />
-      {checked && <button onClick={registration}>회원가입</button>}
+      <Header />
+      <Post {...post} />
     </>
   )
 }
