61042f24e3acb43a4273f8642ac390bf423237ca
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2025-01-14 22:17:09 +0900

    FEAT: [18-2] localStorage 활용하여 다크 / 라이트 테마 저장 및 재사용

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 /*
 실습 18 : Context API 통해 다크 / 라이트 테마에 따른 스타일 변경 및 localStorage 및 이벤트 활용
-[18-1] useContext 통해 다크 / 라이트 테마에 따른 스타일 변경
+[18-2] localStorage 활용하여 다크 / 라이트 테마 저장 및 재사용
 */
 
 import { useState, createContext, useContext, useEffect } from 'react'
@@ -15,19 +15,24 @@ const THEME = {
 const themeContext = createContext({ theme: THEME.DEFAULT, setTheme: (state) => {} })
 
 function ThemeProvider({ children }) {
-  const [theme, setTheme] = useState(THEME.DEFAULT)
+  const stored = localStorage.getItem('theme')
+  const [theme, setTheme] = useState(stored ?? THEME.DEFAULT)
+  // 저장된 값 있으면 그 값으로 초기화.
 
   useEffect(() => {
     if (theme === THEME.DEFAULT) {
       window.matchMedia('(prefers-color-scheme: dark)').matches
         ? document.body.classList.add('dark')
         : document.body.classList.remove('dark')
+      localStorage.setItem('theme', THEME.DEFAULT)
     }
     if (theme === THEME.DARK) {
       document.body.classList.add('dark')
+      localStorage.setItem('theme', THEME.DARK)
     }
     if (theme === THEME.LIGHT) {
       document.body.classList.remove('dark')
+      localStorage.setItem('theme', THEME.LIGHT)
     }
   }, [theme])
 
