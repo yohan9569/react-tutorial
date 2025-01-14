ea56e6d24ece9b006a969ccc592c7b13fa9ad4b2
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2025-01-14 12:24:05 +0900

    FEAT: [13-4] Observer (1) 정의 및 (2) 부착 후 마지막에 (3) 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 /*
 실습 13 : Ref 활용하여, 블로그에서 글 읽을때 어떤 제목의 글인지 상단 내비게이션 헤더에 제목 표기
-[13-3] DOM 에 Observer 부착을 위해 useRef 로 DOM 지정
+[13-4] Observer (1) 정의 및 (2) 부착 후 마지막에 (3) 삭제
 */
 
 import { useState, useRef, forwardRef, useEffect } from 'react'
@@ -9,10 +9,10 @@ import { produce } from 'immer'
 
 function Header({ title }) {
   return (
-    <div
-      style={{ position: 'sticky', top: 0, height: 60, backgroundColor: 'white', color: 'black' }}
-    >
-      {title}
+    <div style={{ position: 'sticky', top: 0, height: 60, backgroundColor: 'white' }}>
+      <div id='header-title' style={{ color: 'black', opacity: 0 }}>
+        {title}
+      </div>
     </div>
   )
 }
@@ -21,8 +21,20 @@ function Title({ title }) {
   const titleRef = useRef(null)
 
   useEffect(() => {
-    console.log(titleRef.current)
-  })
+    console.log(titleRef.current?.innerText)
+
+    // 1. Define: new IntersectionObserver(callback[, options]);
+    const observer = new IntersectionObserver(([entry]) => {
+      document.getElementById('header-title').style.opacity = entry.intersectionRatio ? '0' : '1'
+    })
+    // 2. Attach
+    observer.observe(titleRef.current)
+
+    // 3. Detach: useEffect 클린업 단계. 컴포넌트 언마운트 시 관찰이 중단되고, 메모리가 해제.
+    return () => {
+      observer.disconnect()
+    }
+  }, [])
 
   return <h3 ref={titleRef}>{title}</h3>
 }
